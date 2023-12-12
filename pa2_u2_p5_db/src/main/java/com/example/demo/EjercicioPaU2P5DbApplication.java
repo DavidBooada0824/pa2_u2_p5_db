c88a7b5b64dfcea2b1050e7d67ecbc86a8932e94
DavidBooada0824 <davikbrboada@gmail.com>
2023-12-11 19:41:19 -0500

    taller14

---
+++ pa2_u2_p5_db/src/main/java/com/example/demo/EjercicioPaU2P5DbApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioPaU2P5DbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioPaU2P5DbApplication.class, args);
+	}
+
+}

e79cbd5657f3debc5ef40981c8a00baad815b1f8
DavidBooada0824 <davikbrboada@gmail.com>
2023-12-12 19:47:36 -0500

    taller 15

---
+++ src/main/java/com/example/repo/modelo/Alumno.java
@@ -0,0 +1,42 @@
+package com.example.repo.modelo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name ="alumno")
+public class Alumno {
+	
+	@Id
+	@Column(name = "alum_id")
+	private Integer id;
+	
+	@Column(name = "alum_nombre")
+	private String nombre;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "Alumno [id=" + id + ", nombre=" + nombre + "]";
+	}
+	
+	
+
+}
